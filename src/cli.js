c50d54c17cc51662850ad3e316c415638eed92bb
Kirill Polyakov <thekirill.pol@gmail.com>
2022-01-25 14:14:35 +0300

    fix code eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
-export const greetingUser = () => {
-    console.log('Welcome to the Brain Games!');
-    const yourName = readlineSync.question(`May I have your name? `);
-return console.log(`Hello, ${yourName}!`);
-}
+
+const greetingUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const yourName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello, ${yourName}!`);
+};
+export default greetingUser;
