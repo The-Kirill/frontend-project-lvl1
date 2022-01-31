78e8c2a5b4ddff2409c881535d04970eda73ed62
Kirill Polyakov <thekirill.pol@gmail.com>
2022-01-31 19:39:46 +0300

    fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import getCorrectAnswerAndQuestion from '../src/games/even-game.js';
+import { rule, getCorrectAnswerAndQuestion } from '../src/games/even-game.js';
 import startGame from '../src/index.js';
 
-startGame(getCorrectAnswerAndQuestion);
+startGame(rule, getCorrectAnswerAndQuestion);
