c83f088d76e9f8ddd29ec76c70f74c8dcc21334d
Kirill Polyakov <thekirill.pol@gmail.com>
2022-01-31 19:57:52 +0300

    fix randomNumber.js

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -1,6 +1,6 @@
-const getRandomIntInclusive = () => {
-  const minNum = Math.ceil(1);
-  const maxNum = Math.floor(100);
+const getRandomIntInclusive = (min, max) => {
+  const minNum = Math.ceil(min);
+  const maxNum = Math.floor(max);
   return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 };
 export default getRandomIntInclusive;
