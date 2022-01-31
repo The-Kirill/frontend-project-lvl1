2989a7d5b9bbd87e9201b3840e1867e0fabbf167
Kirill Polyakov <thekirill.pol@gmail.com>
2022-01-31 19:57:13 +0300

    fix logics interaction with user index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import readlineSync from 'readline-sync';
 
-const startGame = (getRaunData) => {
+const startGame = (rule, getRaundData) => {
   console.log('Welcome to the Brain Games!');
   const yourName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${yourName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log(rule);
   const roundCounter = 3;
   for (let i = 0; i < roundCounter; i += 1) {
-    const [questionNum, correctAnswer] = getRaunData();
-    console.log(`Question: ${questionNum}`);
+    const [question, correctAnswer] = getRaundData();
+    console.log(`Question: ${question}`);
     const yourAnswer = readlineSync.question('Yor answer: ');
     if (correctAnswer !== yourAnswer) {
-      console.log(`Let's try again, ${yourName}!`);
+      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${yourName}!`);
       return;
     }
     console.log('Correct!');
