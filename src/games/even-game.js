96157d3b147fd6c244ff0c45b5615088cdbf9bee
Kirill Polyakov <thekirill.pol@gmail.com>
2022-01-31 19:55:09 +0300

    fix logics game even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -3,9 +3,11 @@ import getRandomIntInclusive from '../randomNumber.js';
 
 const isEven = (num) => num % 2 === 0;
 
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const getCorrectAnswerAndQuestion = () => {
-  const questionNum = getRandomIntInclusive();
-  const correctAnswer = isEven(questionNum) ? 'yes' : 'no';
-  return [questionNum, correctAnswer];
+  const question = getRandomIntInclusive(1, 50);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
-export default getCorrectAnswerAndQuestion;
+export { rule, getCorrectAnswerAndQuestion };
