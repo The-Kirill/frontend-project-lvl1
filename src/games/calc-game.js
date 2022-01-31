aaa0f0b3cda724b2a06ea9cdcb4f9866b259c1d3
Kirill Polyakov <thekirill.pol@gmail.com>
2022-01-31 19:53:32 +0300

    add logics game calc-game.js

---
+++ src/games/calc-game.js
@@ -0,0 +1,33 @@
+import getRandomIntInclusive from '../randomNumber.js';
+
+const rule = 'What is the result of the expression?';
+
+const getRandomOperator = () => {
+  const operators = ['+', '-', '*'];
+  let operator = '';
+  const randomIndexOperators = getRandomIntInclusive(0, 2);
+  operator = operators[randomIndexOperators];
+  return operator;
+};
+
+const getCalculate = () => {
+  const randomNumberOne = getRandomIntInclusive(1, 50);
+  const randomNumberTwo = getRandomIntInclusive(1, 50);
+  const randomOperator = getRandomOperator();
+  const question = ` ${randomNumberOne} ${randomOperator} ${randomNumberTwo}`;
+  let correctAnswer;
+  switch (randomOperator) {
+    case '+':
+      correctAnswer = randomNumberOne + randomNumberTwo;
+      break;
+    case '-':
+      correctAnswer = randomNumberOne - randomNumberTwo;
+      break;
+    case '*':
+      correctAnswer = randomNumberOne * randomNumberTwo;
+      break;
+    // no default
+  }
+  return [question, String(correctAnswer)];
+};
+export { rule, getCalculate };
