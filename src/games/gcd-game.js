0620b7e0401595a295d5e1cb71a233783f6d3bc3
Kirill Polyakov <thekirill.pol@gmail.com>
2022-02-01 17:05:44 +0300

    add logics game gcd-game.js

---
+++ src/games/gcd-game.js
@@ -0,0 +1,19 @@
+import getRandomIntInclusive from '../randomNumber.js';
+
+const rule = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return getGcd(b, a % b);
+};
+
+const getCorrectAnswerAndQuestion = () => {
+  const num1 = getRandomIntInclusive(1, 50);
+  const num2 = getRandomIntInclusive(1, 50);
+  const question = `${num1} ${num2}`;
+  const correctAnswer = getGcd(num1, num2).toString();
+  return [question, correctAnswer];
+};
+export { rule, getCorrectAnswerAndQuestion };
