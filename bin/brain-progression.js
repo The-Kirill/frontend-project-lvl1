c453e04add5a2680117bc46886a92aa78d422798
Kirill Polyakov <thekirill.pol@gmail.com>
2022-02-04 15:17:24 +0300

    fix brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/extensions
-import { rule, getCorrectAnswerAndQuestion } from '../src/games/progression-game';
+
+import { rule, getCorrectAnswerAndQuestion } from '../src/games/progression-game.js';
 import startGame from '../src/index.js';
 
 startGame(rule, getCorrectAnswerAndQuestion);
